b267fa4672d3583c0a011a45615999d065ec093f
CosmoHill <cosmohill496@gmail.com>
2013-12-23 02:56:07 +0000

    Update articles.php

--- articles.php
+++ articles.php
@@ -9,16 +9,6 @@ $articles = array(
 	'year' => '42',
 	'imgurl' => '',
 	//'' => '',
-	'body' => 'Welcome to 3301'),
-	array(
-	'title' => 'This is my second article',
-	'subtitle' => 'This is my second subtitle',
-	'datemaj' => 'jan',
-	'datemin' => 'uary',
-	'day' => '01',
-	'year' => '42',
-	'imgurl' => '',
-	//'' => '',
-	'body' => 'nigfag')
+	'body' => 'We'll call that a practice run...'')
 );
 ?>
