f26c267ec9b110890e25bd66dd34794e9766e995
Taiiwo <tylerzacc@gmail.com>
2013-12-23 02:58:19 +0000

    Update articles.php

--- articles.php
+++ articles.php
@@ -9,6 +9,16 @@ $articles = array(
 	'year' => '42',
 	'imgurl' => '',
 	//'' => '',
+	'body' => "We'll call that a practice run..."),
+	array(
+	'title' => 'This is my first article',
+	'subtitle' => 'This is my first subtitle',
+	'datemaj' => 'jan',
+	'datemin' => 'uary',
+	'day' => '01',
+	'year' => '42',
+	'imgurl' => '',
+	//'' => '',
 	'body' => "We'll call that a practice run...")
 );
 ?>
