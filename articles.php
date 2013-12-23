8e03b0227f146201214e0fb2b23f5f97a42300b6
Taiiwo Llort <Tylerzacc@gmail.com>
2013-12-23 02:52:34 +0000

    init

---
+++ articles.php
@@ -0,0 +1,14 @@
+<?php
+$articles = array(
+	array(
+	'title' => 'This is my first article',
+	'subtitle' => 'This is my first subtitle',
+	'datemaj' => 'jan',
+	'datemin' => 'uary',
+	'day' => '01',
+	'year' => '42',
+	'imgurl' => '',
+	//'' => '',
+	'body' => 'Welcome to 3301')
+);
+?>
