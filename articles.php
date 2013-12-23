8d32319adf9142754043725c9989bbd88a0e5477
Taiiwo <tylerzacc@gmail.com>
2013-12-23 03:06:02 +0000

    Update articles.php

--- articles.php
+++ articles.php
@@ -8,6 +8,7 @@ $articles = array(
 	'day' => '01',
 	'year' => '42',
 	'imgurl' => '',
+	'socials' => array('0','1','2','3'),
 	//'' => '',
 	'body' => "We'll call that a practice run..."),
 	array(
@@ -18,6 +19,7 @@ $articles = array(
 	'day' => '01',
 	'year' => '42',
 	'imgurl' => '',
+	'socials' => array('4','5','6','7'),
 	//'' => '',
 	'body' => "We'll call that a practice run...")
 );
