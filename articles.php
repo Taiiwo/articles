40ef14680611703529f45180f16761663f0930dc
Taiiwo <tylerzacc@gmail.com>
2013-12-23 02:55:28 +0000

    Update articles.php

--- articles.php
+++ articles.php
@@ -9,6 +9,16 @@ $articles = array(
 	'year' => '42',
 	'imgurl' => '',
 	//'' => '',
-	'body' => 'Welcome to 3301')
+	'body' => 'Welcome to 3301'),
+	array(
+	'title' => 'This is my second article',
+	'subtitle' => 'This is my second subtitle',
+	'datemaj' => 'jan',
+	'datemin' => 'uary',
+	'day' => '01',
+	'year' => '42',
+	'imgurl' => '',
+	//'' => '',
+	'body' => 'nigfag')
 );
 ?>
