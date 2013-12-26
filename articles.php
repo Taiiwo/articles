01d738f19abaa7db762ab348a54aba28457e5067
Taiiwo <tylerzacc@gmail.com>
2013-12-26 23:17:12 +0000

    Update articles.php

--- articles.php
+++ articles.php
@@ -1,8 +1,8 @@
 <?php
 $articles = array(
 	array(
-	'title' => 'This is my first article',
-	'subtitle' => 'This is my first subtitle',
+	'title' => 'Aoft Vs. The Web',
+	'subtitle' => 'This is why I hate people',
 	'datemaj' => 'jan',
 	'datemin' => 'uary',
 	'day' => '01',
@@ -10,10 +10,19 @@ $articles = array(
 	'imgurl' => '',
 	'socials' => array('0','1','2','3'),
 	//'' => '',
-	'body' => "We'll call that a practice run..."),
+	'body' => "It’s not often I can properly explain the sort of shit I have to put up with constantly, but I finally found a video that can do it:
+
+<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/W8_Kfjo3VjU?rel=0\" frameborder=\"0\" allowfullscreen></iframe>
+
+The saddest part about this is after showing my friends, a large percentage still didn’t get it.
+
+\"You don't know the difference between our website and the internet?\"
+
+Sometimes I wonder how these people found the internet at all without accidentally setting their computer on fire.
+I don’t claim to be a professional, but people lacking basic computer skills seem to be growing impossibly fast in such a technological age. I’m not even sure how it’s possible…"),
 	array(
-	'title' => 'This is my first article',
-	'subtitle' => 'This is my first subtitle',
+	'title' => 'War! What is it good for? Sales.',
+	'subtitle' => '',
 	'datemaj' => 'jan',
 	'datemin' => 'uary',
 	'day' => '01',
@@ -21,6 +30,17 @@ $articles = array(
 	'imgurl' => '',
 	'socials' => array('4','5','6','7'),
 	//'' => '',
-	'body' => "We'll call that a practice run...")
+	'body' => "In the midst of the console war fueled by angry teenagers taking to their computers to vent their ill-created hatred towards huge companies, it’s worth remembering why these arguments are so important.
+
+In any argument, it’s only by one side bringing up points do the other ones even question what they’re shouting about, and these nice, loud and often pointless questions are what get heard.
+
+If a question was loud and smart enough it gathers more voices, more voices snowball into blog posts, blog posts to videos, videos to angry letters and ultimately all of this serves two purposes:
+
+1. Getting the fans in contact with the developers (And the smart developers responding)
+
+2. Free advertising for developers.
+
+However if public relations was a competition, Microsoft would currently be at the starting line putting it’s shoes on the wrong feet. Sony has had it’s teams working tirelessly on social media to answer any questions their fans may have whereas Microsoft have been on social media TELLING their fans what questions they should have.
+Consoles aside, it’s clear to see that America is losing on the charm front, and before release, that’s the only weapon either side has.")
 );
 ?>
